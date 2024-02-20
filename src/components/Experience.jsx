015d913cc7e5820666a824b04bf3ab55de4278db
fancilivain <meigangwen@gmail.com>
2024-02-20 10:39:55 +0800

    added the plane

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,19 @@
-import { OrbitControls } from "@react-three/drei";
+import { Float, OrbitControls } from "@react-three/drei";
+import { Background } from "./Background";
+import { Airplane } from "./Airplane";
 
 export const Experience = () => {
   return (
     <>
       <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <Background />
+      <Float floatIntensity={2} speed={2}>
+        <Airplane
+          rotation-y={Math.PI / 2}
+          scale={[0.2,0.2,0.2]}
+          position-y={0.1} 
+        />
+      </Float>
     </>
   );
 };
